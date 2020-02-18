6625409784f8de4af5cc86ef279f30daea7b6006
JameDy77 <58160702@go.buu.ac.th>
2020-02-18 14:20:23 +0700

    New Function LoopMessage,ConnectRedis

--- feed-facebook-page-wall-master/send_message.go
+++ feed-facebook-page-wall-master/send_message.go
@@ -7,7 +7,7 @@ import (
 	"time"
 
 	"github.com/go-redis/redis" // Redis
-	"github.com/streadway/amqp"
+	"github.com/streadway/amqp" // RabbitMQ
 )
 
 type Rabbit struct {
@@ -20,14 +20,80 @@ type Rabbit struct {
 	Channel    *amqp.Channel
 }
 
+type Redis struct {
+	Host     string `json:"host"`
+	Port     string `json:"port"`
+	Password string `json:"password"`
+	DB       int    `json:"db"`
+	Connect  *redis.Client
+}
+
 func main() {
-	// Redis Client
-	client := redis.NewClient(&redis.Options{
-		// Addr:     "localhost:6379",
-		Addr:     "localhost:32777", // docker
-		Password: "",                // no password set
-		DB:       0,                 // use default DB
-	})
+	time_wait := 15 * 60
+	key := "fb_page_list"
+	access_token := "EAACY2TDTj4kBAPFcGcHaPsF6V6ldbj3iqU72EanY6W142ImRSg51ZAJtGlUZCDIefhixs27fEruqpovqiSPQ7kTWaunZBgrYZAH221DmyNZAhn9m3x5Vw5cU0uL4grOyuNemGTJMx3YZAxnP54LZAgGBuila8WTU7nylZC3ZCSGRgYEfsn8bs9c64eVAdUhJFxueNWWsjlwQ33ZBWB7MmWrgaN"
+	page_id := "100869428117671"
+
+	LoopMessage(time_wait, key, access_token, page_id)
+}
+
+func LoopMessage(time_wait int, key string, access_token string, page_id string) {
+	for {
+		// Redis Connect(Client)
+		redis_struct := Redis{
+			Host:     "localhost",
+			Port:     "32777", // docker port | 6378 default port
+			Password: "",
+			DB:       0,
+		}
+		redis_struct.Connect = redis_struct.ConnectRedis()
+		client := redis_struct.Connect
+
+		//create fb_page_list is map[string]interface for save page_id and access_token
+		var fb_page_list = make(map[string]interface{})
+		fb_page_list["page_id"] = page_id
+		fb_page_list["access_token"] = access_token
+
+		// insert data [key,value] in redis
+		Insert_Key_Value(client, key, fb_page_list)
+
+		// get access_token in redis
+		value_list := Get_Key_Value(client, key)
+
+		// change map[string]interface to json
+		jsonSendMessage, _ := json.Marshal(value_list) // Data from Redis
+
+		// send Message
+		SendMessage(jsonSendMessage)
+
+		// wait send message = time_wait (min)
+		time.Sleep(time.Duration(time_wait) * time.Second)
+	}
+}
+
+func Insert_Key_Value(client *redis.Client, key string, list_data map[string]interface{}) { // insert key field value [field value ...]
+	err := client.HMSet(key, list_data).Err()
+	fmt.Println("---------------------------------------------------------------")
+	if err != nil {
+		// panic(err)
+		log.Println("Can't Insert Data in Redis")
+	} else {
+		log.Println("Insert Data in Redis Successfully")
+	}
+}
+
+func Get_Key_Value(client *redis.Client, key string) map[string]string { // get key field value
+	value_list, err := client.HGetAll(key).Result()
+	if err != nil {
+		// panic(err)
+		log.Println("Can't Select Data in Redis")
+	} else {
+		log.Println("Get Value : ", value_list)
+	}
+	return value_list
+}
+
+func SendMessage(jsonSendMessage []byte) {
 	// Rabbit Connect
 	rabbit := Rabbit{
 		Host:      "localhost",
@@ -38,86 +104,18 @@ func main() {
 	}
 	rabbit.Connection = rabbit.Connect()
 	rabbit.Channel, _ = rabbit.Connection.Channel() // _ = ไม่ใช้ตัวแปรนั้น
-	count_time := 0
-
-	// loop time wait 1 min
-	for {
-		// Redis
-		key := "fb_page_list"
-		access_token := "EAACY2TDTj4kBAPFcGcHaPsF6V6ldbj3iqU72EanY6W142ImRSg51ZAJtGlUZCDIefhixs27fEruqpovqiSPQ7kTWaunZBgrYZAH221DmyNZAhn9m3x5Vw5cU0uL4grOyuNemGTJMx3YZAxnP54LZAgGBuila8WTU7nylZC3ZCSGRgYEfsn8bs9c64eVAdUhJFxueNWWsjlwQ33ZBWB7MmWrgaN"
-		// set access_token
-		var fb = make(map[string]interface{})
-		fb["page_id"] = "100869428117671"
-		fb["access_token"] = access_token
-		err := client.HMSet(key, fb).Err()
-		if err != nil {
-			panic(err)
-		}
-		// get access_token
-		var_fb_check, err := client.HGetAll(key).Result()
-		if err != nil {
-			panic(err)
-		}
-		fmt.Println(len(var_fb_check))
-		fmt.Println(var_fb_check)
-
-		// if len(var_fb_check) == 0 { // insert when no key value
-		// 	var fb = make(map[string]interface{})
-		// 	fb["page_id"] = "100869428117671"
-		// 	fb["access_token"] = access_token
-		// 	err = client.HMSet(key, fb).Err()
-		// 	if err != nil {
-		// 		panic(err)
-		// 	}
-
-		// } else if count_time%3 == 0 || count_time == 0 { // update
-		// 	var fb = make(map[string]interface{})
-		// 	fb["page_id"] = "100869428117671"
-		// 	fb["access_token"] = access_token
-		// 	err = client.HMSet(key, fb).Err()
-		// 	if err != nil {
-		// 		panic(err)
-		// 	}
-
-		// } else if len(var_fb_check) == 2 {
-		// 	var fb = make(map[string]interface{})
-		// 	fb["page_id"] = "100869428117671"
-		// 	fb["access_token"] = access_token
-		// 	err = client.HMSet(key, fb).Err()
-		// 	if err != nil {
-		// 		panic(err)
-		// 	}
-		// }
-
-		// else {
-		// 	fmt.Println(var_fb_check["page_id"])
-		// }
-
-		// end
-		// Rabbit
-		// body := "Hello World!22224341323123"
-		jsonString, _ := json.Marshal(var_fb_check) // Redis
-
-		err = rabbit.Channel.Publish(
-			// "",               // exchange
-			"ha_fbfeed",      // exchange
-			rabbit.QueueName, // routing key
-			false,            // mandatory
-			false,            // immediate
-			amqp.Publishing{
-				// ContentType: "text/plain",
-				// Body:        []byte(body),
-				ContentType: "application/json",
-				Body:        jsonString,
-			})
-		// log.Printf(" [x] Sent %s", body)
-		fmt.Println("----------------------------------------")
-		log.Printf(" [x] Sent %s", var_fb_check)
-		failOnError(err, "Failed to publish a message")
-
-		time.Sleep(900 * time.Second) // wait send message 1 min
-		count_time += 1
-	}
+	err := rabbit.Channel.Publish(
+		// "",               // exchange
+		"ha_fbfeed",      // exchange
+		rabbit.QueueName, // routing key
+		false,            // mandatory
+		false,            // immediate
+		amqp.Publishing{
+			ContentType: "application/json", // type = json
+			Body:        jsonSendMessage,
+		})
+	log.Printf(" [x] SentMessage %s", jsonSendMessage)
+	failOnError(err, "Failed to publish a message")
 }
 
 func failOnError(err error, msg string) {
@@ -141,4 +139,14 @@ func (rabbit Rabbit) Publish(ch *amqp.Channel) {
 	ch, err := rabbit.Connection.Channel()
 	failOnError(err, "Failed to open a channel")
 	defer ch.Close()
+}
+
+func (redis_struct Redis) ConnectRedis() *redis.Client {
+	addr := fmt.Sprintf("%s:%s", redis_struct.Host, redis_struct.Port)
+	client := redis.NewClient(&redis.Options{
+		Addr:     addr,
+		Password: redis_struct.Password,
+		DB:       redis_struct.DB,
+	})
+	return client
 }
