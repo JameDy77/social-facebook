82da9689c2ffed3d0d378c9e9717cef77474407f
JameDy77 <58160702@go.buu.ac.th>
2020-02-18 14:30:38 +0700

    Delete code not used

--- feed-facebook-wall-worker/receive_and_send_message.go
+++ feed-facebook-wall-worker/receive_and_send_message.go
@@ -12,12 +12,6 @@ import (
 	"github.com/streadway/amqp"
 )
 
-func failOnError(err error, msg string) {
-	if err != nil {
-		log.Fatalf("%s: %s", msg, err)
-	}
-}
-
 // send data form page post
 type Social_Data_Facebook struct {
 	Post_id      string    `json:"post_id"`
@@ -30,20 +24,6 @@ type Social_Data_Facebook struct {
 	Status_type  string    `json:"status_type"`
 	Created_time time.Time `json:"created_time"`
 	Updated_time time.Time `json:"updated_time"`
-}
-
-// struct post for stream_facebook
-type stream_facebook struct {
-	Page_id   string `json:"page_id"`
-	Page_name string `json:"page_name"`
-	Data      []data_facebook
-}
-
-// struct data for postpage
-type data_facebook struct {
-	Post_Id, Message, Full_Picture, Story, Status_type string
-	Shares                                             int
-	Created_time, Updated_time                         time.Time
 }
 
 // struct rabbit
@@ -92,113 +72,6 @@ func main() {
 	//
 	hour_stream := 240.00
 	Loop_Message(msgs, rabbit_send, hour_stream)
-
-	// for data := range msgs {
-	// 	fmt.Println("----------------------------------------")
-	// 	log.Printf("[x] Receive")
-	// 	json.Unmarshal([]byte(data.Body), &facebookMap)
-	// 	log.Println("page_id : ", facebookMap["page_id"])
-	// 	log.Println("access_token : ", facebookMap["access_token"])
-
-	// 	// facebook
-	// 	page_id := fmt.Sprintf("%v", facebookMap["page_id"])
-	// 	res, _ := fb.Get("/"+page_id, fb.Params{
-	// 		"fields":       "id,name,feed{message,shares,created_time,updated_time,status_type,story,full_picture}",
-	// 		"access_token": facebookMap["access_token"],
-	// 	})
-	// 	if res["error"] != nil {
-	// 		log.Println("Please new access_totken in fill !!!!!")
-	// 	} else {
-	// 		// page_name := fmt.Sprintf("%v", res["name"])
-	// 		var stream_page []data_facebook
-	// 		if value_feed, has := res["feed"]; has {
-	// 			if value_data, has := value_feed.(map[string]interface{})["data"].([]interface{}); has {
-	// 				for i := 0; i < len(value_data); i++ {
-	// 					data := data_facebook{}
-	// 					// social_data := Social_Data_Facebook{}
-	// 					value := value_data[i].(map[string]interface{})["created_time"]
-	// 					value_str := fmt.Sprintf("%v", value)
-	// 					format := "2006-01-02T15:04:05+0000"
-	// 					location, _ := time.LoadLocation("Asia/Bangkok")
-	// 					createdAt, _ := time.Parse(format, value_str)
-	// 					createdAt = createdAt.In(location)
-
-	// 					expiresAt := time.Now()
-	// 					diff := expiresAt.Sub(createdAt)
-
-	// 					if diff.Hours() >= 0 && diff.Hours() <= 240 { // check data stream 24 hr
-
-	// 						data.Created_time = createdAt
-	// 						data.Post_Id = fmt.Sprintf("%v", value_data[i].(map[string]interface{})["id"])      // * change interface to string
-	// 						data.Message = fmt.Sprintf("%v", value_data[i].(map[string]interface{})["message"]) // * change interface to string
-	// 						shares := fmt.Sprintf("%v", value_data[i].(map[string]interface{})["shares"])       // * change interface to string
-	// 						shares_count, _ := strconv.Atoi(shares)
-	// 						data.Shares = shares_count
-	// 						// updated_time , status_type , story , full_picture
-	// 						updated_time_string := fmt.Sprintf("%v", value_data[i].(map[string]interface{})["updated_time"])
-	// 						updated_time, _ := time.Parse(format, updated_time_string)
-	// 						updated_time = createdAt.In(location)
-	// 						data.Updated_time = updated_time
-	// 						data.Stroy = fmt.Sprintf("%v", value_data[i].(map[string]interface{})["story"])
-	// 						data.Status_type = fmt.Sprintf("%v", value_data[i].(map[string]interface{})["status_type"])
-	// 						data.Full_Picture = fmt.Sprintf("%v", value_data[i].(map[string]interface{})["full_picture"])
-	// 						// add data to stream_page
-	// 						stream_page = append(stream_page, data)
-
-	// 						// ---------------------------------------------------------------------
-	// 						// add social_data to stream fbfeed.post.add
-	// 						// social_data.Created_time = createdAt
-	// 						// social_data.Post_Id = fmt.Sprintf("%v", value_data[i].(map[string]interface{})["id"])      // * change interface to string
-	// 						// social_data.Message = fmt.Sprintf("%v", value_data[i].(map[string]interface{})["message"]) // * change interface to string
-	// 						// shares := fmt.Sprintf("%v", value_data[i].(map[string]interface{})["shares"])              // * change interface to string
-	// 						// shares_count, _ := strconv.Atoi(shares)
-	// 						// social_data.Shares = shares_count
-	// 						// // updated_time , status_type , story , full_picture
-	// 						// updated_time_string := fmt.Sprintf("%v", value_data[i].(map[string]interface{})["updated_time"])
-	// 						// updated_time, _ := time.Parse(format, updated_time_string)
-	// 						// updated_time = createdAt.In(location)
-	// 						// social_data.Updated_time = updated_time
-	// 						// social_data.Stroy = fmt.Sprintf("%v", value_data[i].(map[string]interface{})["story"])
-	// 						// social_data.Status_type = fmt.Sprintf("%v", value_data[i].(map[string]interface{})["status_type"])
-	// 						// social_data.Full_Picture = fmt.Sprintf("%v", value_data[i].(map[string]interface{})["full_picture"])
-
-	// 						// Publish data => queuename = fbfeed.post.add
-	// 						jsonDataStream_facebook, _ := json.Marshal(data) // change stream_facebook struct to json
-	// 						err := rabbit_send.Channel.Publish(
-	// 							// "",               // exchange
-	// 							"ha_fbfeed",           // exchange
-	// 							rabbit_send.QueueName, // routing key
-	// 							false,                 // mandatory
-	// 							false,                 // immediate
-	// 							amqp.Publishing{
-	// 								ContentType: "application/json",
-	// 								Body:        jsonDataStream_facebook,
-	// 							})
-	// 						failOnError(err, "Failed to publish a message")
-	// 					}
-	// 				}
-	// 			}
-	// 		}
-	// 		// stream_facebook := stream_facebook{
-	// 		// 	Page_id:   page_id,
-	// 		// 	Page_name: page_name,
-	// 		// 	Data:      stream_page,
-	// 		// }
-	// 		// // end
-	// 		// jsonStream_facebook, _ := json.Marshal(stream_facebook) // change stream_facebook struct to json
-	// 		// err := rabbit_send.Channel.Publish(
-	// 		// 	// "",               // exchange
-	// 		// 	"ha_fbfeed",           // exchange
-	// 		// 	rabbit_send.QueueName, // routing key
-	// 		// 	false,                 // mandatory
-	// 		// 	false,                 // immediate
-	// 		// 	amqp.Publishing{
-	// 		// 		ContentType: "application/json",
-	// 		// 		Body:        jsonStream_facebook,
-	// 		// 	})
-	// 		// failOnError(err, "Failed to publish a message")
-	// 	}
-	// }
 
 }
 
@@ -318,4 +191,10 @@ func Send_message(social_data Social_Data_Facebook, rabbit_send Rabbit) {
 			Body:        jsonDataStream_facebook,
 		})
 	failOnError(err, "Failed to publish a message")
+}
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+	}
 }
